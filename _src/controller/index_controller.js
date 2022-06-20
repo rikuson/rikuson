4501cba428f11ab8b76a45c5011c676c16bd29a1
Rikson <rikuson@users.noreply.github.com>
2022-06-21 04:36:45 +0900

    Add loader for a post

--- _src/controller/index_controller.js
+++ _src/controller/index_controller.js
@@ -5,15 +5,14 @@ import imagesLoaded from 'imagesloaded';
 import BaseController from './base_controller.js';
 
 jQueryBridget('masonry', Masonry, $);
-jQueryBridget('imagesLoaded', imagesLoaded, $ );
+jQueryBridget('imagesLoaded', imagesLoaded, $);
 
 class IndexController extends BaseController {
   constructor() {
     super();
-    const duration = 500;
     const $container = this.$posts.find('.row');
     $container.imagesLoaded(() => {
-      this.show(this.$posts.find('[data-id]'), duration);
+      this.show(this.$posts.find('[data-id]'));
       $container.masonry();
     });
   }
--- _src/controller/post_controller.js
+++ _src/controller/post_controller.js
@@ -1,8 +1,16 @@
+import $ from 'jquery';
+import jQueryBridget from 'jquery-bridget';
+import imagesLoaded from 'imagesloaded';
 import BaseController from './base_controller.js';
+
+jQueryBridget('imagesLoaded', imagesLoaded, $);
 
 class PostController extends BaseController {
   constructor() {
     super();
+    this.$contents.hide().imagesLoaded(() => {
+      this.show(this.$contents);
+    });
   }
 }
 
