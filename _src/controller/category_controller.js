6e69ce64a63660a4cb95ce1990adfe9e5e629a1d
Riku Takeuchi <rikuson@users.noreply.github.com>
2024-09-24 23:36:14 +0900

    feat: update loader

--- _src/controller/category_controller.js
+++ _src/controller/category_controller.js
@@ -3,7 +3,7 @@ import BaseController from './base_controller.js';
 class CategoryController extends BaseController {
   constructor() {
     super();
-    const $row = this.$posts.find('.row:not(#loader)');
+    const $row = this.$posts.find('.row');
     $row.imagesLoaded(() => {
       this.show($row.children());
       $row.masonry()
