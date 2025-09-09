7046850677997ffaddf0a786cd404482985074e1
Riku Takeuchi <rikuson@users.noreply.github.com>
2025-09-09 09:03:51 +0900

    deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 
 import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify';
 import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://rikson.net',
+	output: 'server',
+	adapter: netlify(),
 	integrations: [mdx(), sitemap(), preact({ compat: true })],
 });
